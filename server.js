e0953ed707f17428919fffca4e86278736b1bcc4
Noah Rosenthal <noahrosenthal@comcast.net>
2020-09-08 14:13:40 -0600

    Minor fix to Proxy

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'));
-app.use('/products/:product_id', express.static('public'));
+app.use('/reviews/*', createProxyMiddleware({ target: 'http://localhost:3001', changeOrigin: true }));
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
